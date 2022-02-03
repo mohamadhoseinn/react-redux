8e648ae93b97c7359dc79a7a22d2225e9a6aa914
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-02-04 00:51:10 +0330

    style in menuItem for hover

--- src/components/MenuItem/MenuItem.component.jsx
+++ src/components/MenuItem/MenuItem.component.jsx
@@ -2,14 +2,15 @@ import "./MenuItem.styles.scss";
 
 const MenuItemComponent = ({ items }) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${items.imageUrl}`,
-      }}
-      className={`${items.size} menu-item`}
-    >
+    <div className={`${items.size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${items.imageUrl}`,
+        }}
+      />
       <div className="content">
-        <h1 className="title">{items.title}</h1>
+        <h1 className="title">{items.title.toUpperCase()}</h1>
         <span className="subtitle">اکنون خرید کنید</span>
       </div>
     </div>
